8fde4200742333a4ff693fe72b9d5677440cacbc
Steve Katra <steve.katra@wolterskluwer.com>
2017-08-28 08:04:05 -0400

    removed blank line

--- src/test/java/com/interview/domain/CheckingAccountTest.java
+++ src/test/java/com/interview/domain/CheckingAccountTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CheckingAccountTest {
-
     @Test
     public void shouldCreateCheckingAccount() throws Exception {
         CheckingAccount checkingAccount = new CheckingAccount();
