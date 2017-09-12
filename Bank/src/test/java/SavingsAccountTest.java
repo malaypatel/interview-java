cd15098e23ee3cad010465ec71d25b717f528d10
NA\justin.smyth <justin.smyth@wolterskluwer.com>
2017-09-12 15:54:05 -0400

    Bank problem initial checkin

---
+++ Bank/src/test/java/SavingsAccountTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class SavingsAccountTest {
+
+	@Test
+	public void testGetBalance() {
+		CheckingAccount checkingAccount = new CheckingAccount();
+		assertNotNull(checkingAccount);
+	}
+	
+
+}
